86e46483649b2a19d1064b88578693746514dbff
Kadir Baran Aksoy <90133005+KadirAksoy@users.noreply.github.com>
2023-06-02 12:05:31 +0300

    try-catch  added

---
+++ src/main/java/org/kadiraksoy/TryCatch.java
@@ -0,0 +1,24 @@
+package org.kadiraksoy;
+
+import java.util.Scanner;
+
+public class TryCatch {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            System.out.print("Enter a number: ");
+            int number = scanner.nextInt();
+
+            int result = 10 / number;
+            System.out.println("Result: " + result);
+        } catch (ArithmeticException e) {
+            System.out.println("An error occurred: " + e.getMessage());
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
+        } finally {
+            scanner.close();
+        }
+    }
+}
